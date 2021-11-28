ea6c934f44d546f7fabc149f7ff5f300e153e2c2
Michel Erokhov <mixaileroxov@gmail.com>
2021-11-28 16:55:57 +0300

    add helpers

---
+++ src/helpers/index.ts
@@ -0,0 +1,5 @@
+export * from '@/helpers/isTesting'
+export * from '@/helpers/jwt'
+export * from '@/helpers/openApi'
+export * from '@/helpers/validate'
+export * from '@/helpers/validateMiddleware'
---
+++ src/helpers/validateMiddleware.ts
@@ -0,0 +1,14 @@
+import { ClassConstructor } from 'class-transformer'
+import { Context, Next } from 'koa'
+import validate from '@/helpers/validate'
+
+export default function validateParamsMiddleware<T extends object>(
+  classConstructor: ClassConstructor<T>
+) {
+  return (middleware: (ctx: Context, next: Next) => void) => {
+    return async (ctx: Context, next: Next) => {
+      await validate(ctx, classConstructor, ctx.params, 'state')
+      return middleware(ctx, next)
+    }
+  }
+}
