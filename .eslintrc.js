40e1e375d02edcbfc37289679edbe1c2443b6144
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-06-19 23:08:16 +0400

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,9 @@
-require('@rushstack/eslint-patch/modern-module-resolution')
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
   extends: ['./node_modules/@big-whale-labs/bwl-eslint-backend'],
   parserOptions: { tsconfigRootDir: __dirname },
-}
+  rules: {
+    'no-relative-import-paths/no-relative-import-paths': 'off',
+  },
+};
