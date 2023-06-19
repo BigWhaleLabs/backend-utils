fb7591877ed16b02d463e6a878cffdc1c5a77fd2
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-06-19 22:52:10 +0400

    fix exports

--- src/helpers/generateRandomName.ts
+++ src/helpers/generateRandomName.ts
@@ -2,7 +2,7 @@ import { colorDictionary, nounDictionary } from '@/data/nameDictionaries'
 import { createHash } from 'crypto'
 import capitalizeFirstLetter from '@/helpers/capitalizeFirstLetter'
 
-export default function generateRandomName(address: string) {
+export function generateRandomName(address: string) {
   const hashed = createHash('sha256').update(address).digest('hex')
   const hashedBigint = BigInt('0x' + hashed)
   const firstWordIndex = Number(hashedBigint % BigInt(colorDictionary.length))
@@ -11,3 +11,5 @@ export default function generateRandomName(address: string) {
   const secondWord = capitalizeFirstLetter(nounDictionary[secondWordIndex])
   return `${firstWord}${secondWord}`
 }
+
+export default generateRandomName
