8c1fe59b262c16dfb16777e040b9191dc1b30af3
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-06-19 22:11:40 +0400

    add generating random nicknames

---
+++ src/helpers/generateRandomName.ts
@@ -0,0 +1,13 @@
+import { colorDictionary, nounDictionary } from '@/data/nameDictionaries'
+import { createHash } from 'crypto'
+import capitalizeFirstLetter from '@/helpers/capitalizeFirstLetter'
+
+export default function generateRandomName(address: string) {
+  const hashed = createHash('sha256').update(address).digest('hex')
+  const hashedBigint = BigInt('0x' + hashed)
+  const firstWordIndex = Number(hashedBigint % BigInt(colorDictionary.length))
+  const secondWordIndex = Number(hashedBigint % BigInt(nounDictionary.length))
+  const firstWord = capitalizeFirstLetter(colorDictionary[firstWordIndex])
+  const secondWord = capitalizeFirstLetter(nounDictionary[secondWordIndex])
+  return `${firstWord}${secondWord}`
+}
