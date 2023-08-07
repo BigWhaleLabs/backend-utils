79c74770254c7db508dc5fc32bbaf105185015e1
ruslan zapevalov <ruslan@bigwhalelabs.com>
2023-08-07 23:04:35 +0400

    change dictionary

--- src/helpers/generateRandomName.ts
+++ src/helpers/generateRandomName.ts
@@ -1,14 +1,14 @@
-import { colorDictionary, nounDictionary } from '../data/nameDictionaries'
+import { adjectives, nouns } from '../data/nameDictionaries'
 import { createHash } from 'crypto'
 import capitalizeFirstLetter from './capitalizeFirstLetter'
 
 export function generateRandomName(address: string) {
   const hashed = createHash('sha256').update(address).digest('hex')
   const hashedBigint = BigInt('0x' + hashed)
-  const firstWordIndex = Number(hashedBigint % BigInt(colorDictionary.length))
-  const secondWordIndex = Number(hashedBigint % BigInt(nounDictionary.length))
-  const firstWord = capitalizeFirstLetter(colorDictionary[firstWordIndex])
-  const secondWord = capitalizeFirstLetter(nounDictionary[secondWordIndex])
+  const firstWordIndex = Number(hashedBigint % BigInt(adjectives.length))
+  const secondWordIndex = Number(hashedBigint % BigInt(nouns.length))
+  const firstWord = capitalizeFirstLetter(adjectives[firstWordIndex])
+  const secondWord = capitalizeFirstLetter(nouns[secondWordIndex])
   return `${firstWord}${secondWord}`
 }
 
